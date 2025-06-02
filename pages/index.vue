de52cd842fe7895b4df8027f789e1b9d5bc7eb4e
Aaron Schmied <aaron@schmied.dev>
2025-06-03 00:46:14 +0200

    Added start links

--- pages/index.vue
+++ pages/index.vue
@@ -20,6 +20,13 @@
           Tippen Sie auf den Button, sobald Sie bereit sind:
         </p>
       </div>
+
+      <div class="text-center mt-10">
+        <NuxtLink to="/steps/og1/batteries"
+                  class="mt-20 text-2xl bg-red-500 text-white px-8 py-4 rounded hover:bg-red-600 transition-colors">
+          Start
+        </NuxtLink>
+      </div>
     </template>
     <template #group-c>
       <div class="prose prose-lg prose-invert mt-40">
@@ -27,12 +34,26 @@
           Tippen Sie auf den Button, sobald Sie bereit sind:
         </p>
       </div>
+
+      <div class="text-center mt-10">
+        <NuxtLink to="/steps/og2/it"
+                  class="mt-20 text-2xl bg-red-500 text-white px-8 py-4 rounded hover:bg-red-600 transition-colors">
+          Start
+        </NuxtLink>
+      </div>
     </template>
     <template #group-d>
       <div class="prose prose-lg prose-invert mt-40">
         <p>
           Tippen Sie auf den Button, sobald Sie bereit sind:
         </p>
+      </div>
+
+      <div class="text-center mt-10">
+        <NuxtLink to="/steps/og3/helppoint"
+                  class="mt-20 text-2xl bg-red-500 text-white px-8 py-4 rounded hover:bg-red-600 transition-colors">
+          Start
+        </NuxtLink>
       </div>
     </template>
 
