a41a6d40cc8a50f87f5a6941b521d0cad69e3db0
Aaron Schmied <aaron@schmied.dev>
2025-06-02 23:07:21 +0200

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-05-15',
     devtools: {enabled: true},
     css: ['~/assets/css/main.css'],
+    ssr: false,
+    app: {
+        baseURL: '/escaperoom/'
+    },
     vite: {
         plugins: [
             tailwindcss(),
