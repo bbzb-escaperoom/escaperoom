e6e0f86f20bb536527369bc4dba2af2951b6b41b
Aaron Schmied <aaron@schmied.dev>
2025-06-02 22:56:28 +0200

    wip

---
+++ app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="fixed inset-0 bg-grainy opacity-30 z-50"/>
+
+  <div class="max-w-lg mx-auto p-4 text-white">
+    <NuxtPage/>
+  </div>
+</template>
+
+<style>
+
+body {
+  background: #000000;
+}
+
+.bg-grainy {
+  background: url(https://grainy-gradients.vercel.app/noise.svg);
+  animation: bg-grainy 2s infinite alternate;
+}
+
+@keyframes bg-grainy {
+  from {
+    filter: blur(0);
+    background-position: 0 0;
+  }
+  to {
+    filter: blur(1px);
+    background-position: 10px 10px;
+  }
+}
+
+</style>
---
+++ pages/steps/og1/start.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="prose prose-lg prose-invert">
+
+    <p>
+      Diese zwei Rätsel ergeben je ein Lösungswort. Wenn Sie die beiden Lösungsworte zusammenführen, erhalten Sie den
+      Hinweis, nach was Sie im 1.OG suchen sollen:
+    </p>
+
+    <p>
+      „Ich bin winzig klein, eine Masseinheit, die man kaum sieht, doch in Technik und Forschung man mich genießt.“
+    </p>
+
+    <p>
+      „Mal hoch, mal tief, ich tanze im Meer, doch auch unsichtbar trag ich Geräusche umher.“
+    </p>
+
+    <p>
+      Lösungswort 1 + Lösungswort 2 = _ _ _ _ _ _ _ _
+    </p>
+
+    <p>
+      Suchen Sie den Ort und scannen Sie den richtigen QR-Code, um weiterzukommen.
+    </p>
+  </div>
+
+</template>
