8e9220f7db707c6c81352125ef580981e425a3ce
Aaron Schmied <aaron@schmied.dev>
2025-06-04 23:56:15 +0200

    Added page for wrong answers

---
+++ pages/wrong.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <div class="prose prose-lg prose-invert my-12">
+    <h1>falsch</h1>
+
+    <p>Bedeutungen:</p>
+    <ol>
+      <li>unrichtig (durch Irrtum oder Unwissenheit), tatsächlich anders</li>
+      <li>gelogen, nicht den Tatsachen entsprechend</li>
+      <li>beim Spiel: betrügerisch</li>
+      <li>nachgemacht</li>
+    </ol>
+    <p class="font-bold">Dies war leider nicht die richtige Antwort. Versuchen Sie einen anderen Weg.</p>
+  </div>
+</template>
